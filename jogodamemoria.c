808e00fb8f56c330eba1ab2e6849fd5d468beb98
micaels94 <96264952+micaels94@users.noreply.github.com>
2024-11-13 13:58:33 +0000

    Add files via upload

---
+++ jogodamemoria.c
@@ -0,0 +1,37 @@
+// Há uma tabela de cartas que contém cartas do 0 ao 63 do 
+// correspondem simbolos "+ | * | / | - ", 4 letras A, B, C, D
+// Por exemplo quando o user escolhe o codigo "0" 
+// é retornado +1A
+
+// vai ser necessário: 
+/* 
+- Uma função para o input do utilizador e guardar o valor
+- Uma função com uma variável que retorna o input do usuário
+
+
+*/
+
+
+#include <stdio.h>
+
+int baralho(){
+    char cartas[cartas.length] = {}
+
+
+    for (int i = 0; i <= 63; i++){
+        for (int j = 1; j <= 4; j++){
+            // a ideia aqui é um loop de simbolos de operação até ao 16, para cada associar um
+            // número de 1 a 4 e depois uma letra do A a D 
+            
+        }
+    }
+}
+
+
+
+
+int main (){
+
+
+
+}
